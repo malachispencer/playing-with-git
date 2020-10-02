89bf441be9f9beebd368c7217a11bced07b81822
Malachi Spencer <71923215+malachispencer@users.noreply.github.com>
2020-10-02 10:28:12 +0200

    JS functions for current time 24/12 hour formats

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-function currentTime() {
-  const hours = (new Date().getHours() < 10 ? '0' : '') + new Date().getHours();
-  const minutes = (new Date().getMinutes() < 10 ? '0' : '') + new Date().getMinutes();
-  return `The time is ${hours}:${minutes}`;
+
+function currentTime24hrs() {
+  const date = new Date();
+  const hours = (date.getHours() < 10 ? '0' : '') + date.getHours();
+  const minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+  return `${hours}:${minutes}`;
 }
 
-console.log(currentTime());
+const to12hrs = {13: 1, 14: 2, 15: 3, 16: 4, 17: 5, 18: 6, 19: 7, 20: 8, 21: 9, 22: 10, 23: 11, 0: 12};
+
+function currentTime12hrs() {
+  const date = new Date();
+  let hours = date.getHours();
+  const suffix = hours < 12 ? 'am' : 'pm';
+  if (to12hrs[hours]) {hours = to12hrs[hours];}
+  const minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+  return `${hours}:${minutes}${suffix}`;
+}
+
+console.log(currentTime24hrs());
+console.log(currentTime12hrs());
