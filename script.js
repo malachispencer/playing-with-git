6bfed418ae1961b065e4f503e2b5b558826aee23
Malachi Spencer <71923215+malachispencer@users.noreply.github.com>
2020-12-29 15:56:52 +0000

    Removed console.logs

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function currentTime24hrs() {
   const date = new Date();
   const hours = (date.getHours() < 10 ? '0' : '') + date.getHours();
@@ -6,16 +5,28 @@ function currentTime24hrs() {
   return `${hours}:${minutes}`;
 }
 
-const to12hrs = {13: 1, 14: 2, 15: 3, 16: 4, 17: 5, 18: 6, 19: 7, 20: 8, 21: 9, 22: 10, 23: 11, 0: 12};
+const to12hrs = {
+  13: 1, 
+  14: 2, 
+  15: 3, 
+  16: 4, 
+  17: 5, 
+  18: 6, 
+  19: 7, 
+  20: 8, 
+  21: 9, 
+  22: 10, 
+  23: 11, 
+  0: 12
+};
 
 function currentTime12hrs() {
   const date = new Date();
   let hours = date.getHours();
   const suffix = hours < 12 ? 'am' : 'pm';
+  
   if (to12hrs[hours]) {hours = to12hrs[hours];}
+  
   const minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
   return `${hours}:${minutes}${suffix}`;
 }
-
-console.log(currentTime24hrs());
-console.log(currentTime12hrs());
